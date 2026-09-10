7e7ee12348fab4e54bbf0430e3698ef32dc67b7f
akmalfarsana <farsana081224@gmail.com>
2026-09-10 11:11:36 +0530

    Update arraysum.c

--- farsana ads/arraysum.c
+++ farsana ads/arraysum.c
@@ -1,25 +1,25 @@
  #include<stdio.h>
-  2 int arraysum(int n,int arr[50])
-  3 {
-  4     int i,sum=0;
-  5     for(i=0;i<n;i++)
-  6     {
-  7         sum=sum+arr[i];
-  8     }
-  9     return sum;
- 10 }
- 11 int main()
- 12 {
- 13     int i,n,arr[50];
- 14     printf("enter number of elements:");
- 15     scanf("%d",&n);
- 16     printf("enter %d elements:", n);
- 17     for(i=0;i<n;i++)
- 18     {
- 19     scanf("%d",&arr[i]);
- 20     }
- 21     printf("sum=%d",arraysum(n,arr));
- 22 }
+ int arraysum(int n,int arr[50])
+ {
+      int i,sum=0;
+      for(i=0;i<n;i++)
+      {
+          sum=sum+arr[i];
+      }
+      return sum;
+  }
+  int main()
+  {
+      int i,n,arr[50];
+      printf("enter number of elements:");
+      scanf("%d",&n);
+      printf("enter %d elements:", n);
+      for(i=0;i<n;i++)
+      {
+      scanf("%d",&arr[i]);
+      }
+      printf("sum=%d",arraysum(n,arr));
+  }
 
 
 output:
