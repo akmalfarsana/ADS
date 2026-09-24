742fa959f2084ae4202131d70fc5e0d41710692b
akmalfarsana <farsana081224@gmail.com>
2026-09-24 11:00:41 +0530

    Update lrg.c

--- lrg.c
+++ lrg.c
@@ -1,20 +1,20 @@
    #include<stdio.h>
-  2 void lgarray()
-  3 {
-  4     int arr[],i,n;
-  5     int max=0;
-  6     printf("enter limit:");
-  7     scanf("%d",&n);
-  8     printf("enter elements:")
-  9     for(i=0;i<n;i++)
- 10     {
- 11         scanf("%d",&arr[i]);
- 12         if(arr[i]>max){
- 13             max=arr[i];
- 14         }
- 15     }
- 16     printf("greatest = ",max);
- 17 }
- 18 void main()
- 19 {
+   void lgarray()
+   {
+       int arr[],i,n;
+       int max=0;
+       printf("enter limit:");
+       scanf("%d",&n);
+       printf("enter elements:")
+       for(i=0;i<n;i++)
+      {
+          scanf("%d",&arr[i]);
+          if(arr[i]>max){
+              max=arr[i];
+          }
+      }
+      printf("greatest = ",max);
+  }
+  void main()
+  {
 
